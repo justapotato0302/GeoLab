73c3624cfe18fa0589eea8a9cf30d9b1f5e68942
chusonnguyen <s3742891@rmit.edu.vn>
2021-04-12 16:14:19 +0700

    redirect to login page

--- app/src/main/java/com/example/geolab/MainActivity.kt
+++ app/src/main/java/com/example/geolab/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.geolab
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button : Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
